d079616d64f4521d6d392ce494c85510c3c5769f
editmond <42686022+editmond@users.noreply.github.com>
2024-07-26 10:07:53 +1000

    asdf

--- Simplex/Views/Sidebar/SettingsWindow/AudioSettingsView.swift
+++ Simplex/Views/Sidebar/SettingsWindow/AudioSettingsView.swift
@@ -13,34 +13,32 @@ struct AudioSettingsView: View {
     
     var body: some View {
         VStack{
-            HStack{
-                Text("Volume: ")
-                    .bold()
-                    .padding()
-                Spacer()
-                Slider(value: $playerObject.retainedVolume, in: 0...1)
-                    .padding()
-                Text("\(playerObject.retainedVolume)")
-                Picker("Skip Forward Amount", selection: $playerObject.skipForwardAmount){
-                    Text("5").tag(skipAmount.five)
-                    Text("10").tag(skipAmount.ten)
-                    Text("15").tag(skipAmount.fifteen)
-                    Text("30").tag(skipAmount.thirty)
-                    Text("45").tag(skipAmount.fourtyFive)
-                    Text("60").tag(skipAmount.sixty)
-                    Text("75").tag(skipAmount.seventyFive)
-                    Text("90").tag(skipAmount.ninety)
-                }
-                Picker("Skip Backward Amount", selection: $playerObject.skipBackwardAmount){
-                    Text("5").tag(skipAmount.five)
-                    Text("10").tag(skipAmount.ten)
-                    Text("15").tag(skipAmount.fifteen)
-                    Text("30").tag(skipAmount.thirty)
-                    Text("45").tag(skipAmount.fourtyFive)
-                    Text("60").tag(skipAmount.sixty)
-                    Text("75").tag(skipAmount.seventyFive)
-                    Text("90").tag(skipAmount.ninety)
-                }
+            Text("Volume: ")
+                .bold()
+                .padding()
+            Spacer()
+            Slider(value: $playerObject.retainedVolume, in: 0...1)
+                .padding()
+            Text("\(playerObject.retainedVolume)")
+            Picker("Skip Forward Amount", selection: $playerObject.skipForwardAmount){
+                Text("5").tag(skipAmount.five)
+                Text("10").tag(skipAmount.ten)
+                Text("15").tag(skipAmount.fifteen)
+                Text("30").tag(skipAmount.thirty)
+                Text("45").tag(skipAmount.fourtyFive)
+                Text("60").tag(skipAmount.sixty)
+                Text("75").tag(skipAmount.seventyFive)
+                Text("90").tag(skipAmount.ninety)
+            }
+            Picker("Skip Backward Amount", selection: $playerObject.skipBackwardAmount){
+                Text("5").tag(skipAmount.five)
+                Text("10").tag(skipAmount.ten)
+                Text("15").tag(skipAmount.fifteen)
+                Text("30").tag(skipAmount.thirty)
+                Text("45").tag(skipAmount.fourtyFive)
+                Text("60").tag(skipAmount.sixty)
+                Text("75").tag(skipAmount.seventyFive)
+                Text("90").tag(skipAmount.ninety)
             }
         }
         .navigationTitle("Settings")
